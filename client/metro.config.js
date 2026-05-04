6097d3d5010360c6b300fe9c6b4f9d351495ae07
Vanliving User <user@example.com>
2026-05-04 13:05:29 +0800

    fix: 移除uniwind插件配置，解决构建问题

--- client/metro.config.js
+++ client/metro.config.js
@@ -1,6 +1,5 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const { withUniwind } = require('uniwind/metro');
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withUniwind(config);
+module.exports = config;
